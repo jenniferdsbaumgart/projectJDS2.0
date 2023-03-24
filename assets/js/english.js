8856369bc05749b7639598f1e302d12d4c4be6cb
Jennifer Da Silva <jenniferdsbaumgart@gmail.com>
2023-03-24 18:15:36 -0300

    english version

--- assets/js/english.js
+++ assets/js/english.js
@@ -16,13 +16,13 @@ $(document).ready(function(){
    });
 
    var typed = new Typed(".typing", {
-       strings:["desenvolvedora front end"],
+       strings:["front end developer"],
        typeSpeed:100,
        backSpeed:60,
        loop:true
    });
    var typed = new Typed(".typing-2", {
-      strings:["desenvolvedora front end"],
+      strings:["front end developer"],
       typeSpeed:100,
       backSpeed:60,
       loop:true
